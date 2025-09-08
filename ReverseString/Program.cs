97266ddf4f3e22ea94c8a6635c7641e2058b49b7
Peter Baggetta <petebaggetta@gmail.com>
2025-09-07 22:13:16 -0400

    Implemented palindrome check and added option to continue playing the game

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -7,29 +7,40 @@
             // Introduction text
             Console.WriteLine("Welcome to the Reverse String Program. Enter a word, and I will give it to you in reverse!");
 
-            // Ask for user input
-            Console.WriteLine("Please enter a word: ");
-            string userInput = Console.ReadLine();
-
-            // Declar a charater array so we can iterate through the string 
-            char[] reversed = new char[userInput.Length];
-
-            // Fill the charater array with the reverse user input
-            for (int i = 0; i < userInput.Length; i++)
+            while (true)
             {
-                reversed[i] = userInput[userInput.Length - i - 1];
-            }
+                // Ask for user input
+                Console.WriteLine("\nPlease enter a word: ");
+                string userInput = Console.ReadLine();
 
-            // Turn the character array into a string to be printed
-            string result = new string(reversed);
+                // Declar a charater array so we can iterate through the string 
+                char[] reversed = new char[userInput.Length];
 
-            // Output reversed string
-            Console.WriteLine($"The reversed string is: {result}");
+                // Fill the charater array with the reverse user input
+                for (int i = 0; i < userInput.Length; i++)
+                {
+                    reversed[i] = userInput[userInput.Length - i - 1];
+                }
 
-            // Check for Palindrome
-            if (userInput == result)
-            {
-                Console.WriteLine("This is a Palindrome!");
+                // Turn the character array into a string to be printed
+                string result = new string(reversed);
+
+                // Output reversed string
+                Console.WriteLine($"The reversed string is: {result}");
+
+                // Check for Palindrome
+                if (userInput.ToLower() == result.ToLower())
+                {
+                    Console.WriteLine("This is a Palindrome!");
+                }
+
+                Console.WriteLine("Want to play again? (Y/N): ");
+                string playAgain = Console.ReadLine().ToLower();
+                if (playAgain == "n")
+                {
+                    Console.WriteLine("Thank you for playing!");
+                    break;
+                }
             }
         }
     }
